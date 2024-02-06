bb610dfabc9301ca4ab533c535d1a101c69bacb0
Rashminda <rashminda121@gmail.com>
2024-02-06 01:25:10 +0000

    added background

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import "./App.scss";
 
 function App() {
   return (
-    <hgroup>
+    <article>
+      <hgroup>
       <img src="images/group.svg" alt="Star Gazers" />
       <h1>Meet the StarGazers</h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+      </hgroup>
+    </article>
   )
 }
 export default App
