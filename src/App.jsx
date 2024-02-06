93b69b7d87436b82f9351b60ef26bed6c5a62a86
Rashminda <rashminda121@gmail.com>
2024-02-06 01:35:53 +0000

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       <img src="images/group.svg" alt="Star Gazers" />
       <h1>Meet the <i>{name}</i> </h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        <button className='outline' onClick={()=>alert('Hellooo')}>Click Me</button>
       </hgroup>
     </article>
   )
