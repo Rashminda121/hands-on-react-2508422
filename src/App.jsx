775a9c937cc05648efecae92a6a599ecfca5c9c7
Rashminda <rashminda121@gmail.com>
2024-02-06 01:31:32 +0000

    added variable

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Nav from './components/Nav';
 import "./App.scss";
 
 function App() {
+
+  const name='StarGazers';
   return (
     <article>
       <hgroup>
       <img src="images/group.svg" alt="Star Gazers" />
-      <h1>Meet the StarGazers</h1>
+      <h1>Meet the <i>{name}</i> </h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
       </hgroup>
     </article>
